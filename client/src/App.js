5e4b96429ed144387882ce90f9268bc996374d9c
Brian Paintsil <brnpaintsil@gmail.com>
2020-09-15 17:19:23 +0000

    add spinner

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import {
-    Button,
-} from 'reactstrap';
+import { Button, Spinner } from 'reactstrap';
 import TodoTable from './table';
 import BasePage from './BasePage';
 import { getAllTodos } from './todoservice';
 
 function App() {
     const [isOpen, setIsOpen] = useState(false);
-    const [apiTodos, setApiTodos] = useState(null)
-    const [loading, setLoading] = useState(false)
+    const [apiTodos, setApiTodos] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     const fetchTodos = () => {
-      setLoading(true)
-      getAllTodos().then((todos) => {
-          setApiTodos(todos)
-      }).then(() => setLoading(false)).catch(() => {
-        setLoading(false)
-        setApiTodos([])
-      })
+        setLoading(true);
+        getAllTodos()
+            .then((todos) => {
+                setApiTodos(todos);
+            })
+            .then(() => setLoading(false))
+            .catch(() => {
+                setLoading(false);
+                setApiTodos([]);
+            });
     };
 
     useEffect(() => {
-      fetchTodos();
-    }, [])
+        fetchTodos();
+    }, []);
 
     return (
         <BasePage>
-            <Link to="/create"><Button>Add Todo</Button></Link>
+            <div>
+                <Link to='/create'>
+                    <Button>Add Todo</Button>
+                </Link>
+                {loading && <Spinner color='dark' className="position-absolute" />}
+            </div>
+
             <TodoTable todos={apiTodos} />
         </BasePage>
     );
